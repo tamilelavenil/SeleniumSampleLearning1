97c7f98530b209bc7a40858d35845f332959b86f
tamilelavenil <93430431+tamilelavenil@users.noreply.github.com>
2021-11-10 23:30:58 +0530

    Create FindContactsUsingMail.java

---
+++ FindContactsUsingMail.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FindContactsUsingMail {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");;
+		driver.findElement(By.id("password")).sendKeys("crmsfa");;
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Contacts")).click();
+		driver.findElement(By.linkText("Find Contacts")).click();
+		driver.findElement(By.xpath("//span[text()='Email']")).click();
+		driver.findElement(By.xpath("//span[text()='Email']")).click();
+		driver.findElement(By.xpath("//input[@name='emailAddress']")).sendKeys("babu@testleaf.com");
+		driver.findElement(By.xpath("//button[text()='Find Contacts']")).click();
+		driver.close();
+	}
+
+}
+
